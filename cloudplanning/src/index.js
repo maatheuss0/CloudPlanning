eb7143036329d73433c8c9d1b73f1379e3ac3e9a
Matheus De Araujo Machado <47524754809@info.scs>
2022-03-29 08:44:46 -0300

    Front-end Inciail

---
+++ cloudplanning/src/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {
+  Route,
+  BrowserRouter as Router,
+  Redirect,
+  Switch,
+} from 'react-router-dom';
+
+import './index.css';
+
+import Home from './pages/home/App';
+import Login from './pages/login/login';
+
+import reportWebVitals from './reportWebVitals';
+
+
+const routing = (
+  <Router>
+    <div>
+      <Switch>
+        <Route exact path="/" component={Home} /> {/* Home */}
+        <Route path="/Login" component={Login} /> {/* Login */}
+      </Switch>
+    </div>
+  </Router>
+)
+
+
+ReactDOM.render(
+  routing,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
