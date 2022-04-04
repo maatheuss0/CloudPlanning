e70564af32ec5736a16e3f34882d67155701bbf7
maatheuss0 <matheusaraujoms.machado@gmail.com>
2022-04-03 23:28:26 -0300

    Alterações front

--- cloudplanning/src/pages/home/App.js
+++ cloudplanning/src/pages/home/App.js
@@ -1,40 +1,94 @@
-import './App.css'
 
-import logo from '../../assets/img/CloudPlanning-_1_-1 1.png'
-import empresa1 from '../../assets/img/DAREDE.png'
+
+import './App.css';
+
+import logo '../../assets/img';
+import darede '../../assets/img';
+import aws '../../assets/img';
+import diagrama1 '../../assets/img';
+import diagrama1 '../../assets/img';
+import diagrama '../../assets/img';
+import '../../assets/img';
 
 function App() {
-    return (
-
-        <>
-            <header>
-                <div class="container container_header">
-                    <img src={logo} alt="logo_CloudPlanning"/>
-                    <p>CloudPlanning</p>
-                    <nav class ="cabeçalho">
-                    <a class ="alinhamento" href="#início">Início</a>
-                    <a class ="alinhamento" href="#sobre nós">Sobre nós</a>
-                    <a class ="login" href="#login">LOGIN</a>
-                    <button>Cadastrar</button>
-                    </nav>
+  return (
+    <div>
+      <div class="fundo">
+        <section class="container_banner container  banner">
+          <div class="conteudo_banner">
+            <div>
+              <article class="caixa">
+                <div class="alinhar">
+                  <img class="img1" src="assets/img/DAREDE.png" alt="artigo">
+                    <p>Com vasta experiência no mercado e sendo parceira da Amazon
+                      Web Services e Microsoft, duas das principais empresas de tecnologia do mundo, a
+                      Darede
+                      busca trazer inovação e excelência ao oferecer a seus clientes os melhores
+                      serviços
+                      do
+                      mercado. A Darede oferece uma orientação completa e abrangente para que sua
+                      empresa
+                      siga a melhor jornada para cloud, projetando uma arquitetura personalizada e
+                      totalmente voltada para suas necessidades</p>
                 </div>
-            </header>
+              </article>
+            </div>
+            <div class="caixa">
+              <div class="alinhar"></div>
+              <img class="img2" src="assets/img/aws.png" alt="artigo">
+                <p>A Amazon Web Services (AWS) é a plataforma de nuvem mais adotada e mais abrangente do
+                  mundo,
+                  oferecendo mais de 200 serviços completos de datacenters em todo o mundo. Os recursos de
+                  computação em nuvem da Amazon são hospedados em vários locais no mundo todo. Esses
+                  locais
+                  são compostos por regiões da AWS, zonas de disponibilidade e zonas locais. Cada região
+                  da
+                  AWS é uma área geográfica separada</p>
+            </div>
 
-            <main>
-                <section class="container_banner banner">
-                    <div class="conteudo_banner">
-                        <div class="caixa">
-                            <img src={empresa1} alt="logo_darede"/>
-                            <p>da rede</p>
-                        </div>
+          </div>
+        </section>
+      </div>
+
+      <section class="diagramas container">
+        <h2>Trabalhe como um profissional</h2>
+        <h3>Crie diagramas da AWS</h3>
+        <button>Crie seu diagrama</button>
+        <hr>
+          <div class="infra">
+            <img class="infra2" src="assets/img/pngwing 1.png" alt="">
+              <img src="assets/img/pngwing 3.png" alt="">
+                <img class="infra2" src="assets/img/pngwing 2.png" alt="">
+                </div>
+              </section>
+
+              <section class="container">
+                <div class="fundo2">
+                  <img src="assets/img/pngwing 2.png" alt="">
+                    <div class="quem_somos">
+                      <span>Os diagramas de rede são essenciais para ilustrar as entradas e saídas da sua rede. Como
+                        engenheiro de rede ou profissional de TI, você provavelmente precisará desenhar diagramas de
+                        rede ao projetar, desenvolver e solucionar problemas de sua rede, ou para ajudar outras pessoas
+                        a entender suas funcionalidades. Programas para desenhar estrutura de redes também podem ser
+                        usados para coordenar atualizações e atuar como documentações para a comunicação externa,
+                        integração e solução de problemas
+                      </span>
+                      <span>
+                        Um diagrama de rede é uma representação visual de uma rede de computadores ou telecomunicações.
+                        Ele mostra os componentes que constituem uma rede e como eles interagem, incluindo roteadores,
+                        dispositivos, hubs, firewalls etc. Este diagrama de rede mostra uma rede de área local
+                        (LAN)</span>
+                      <button>Começe agora</button>
                     </div>
-                </section>
-            </main>
+                </div>
+              </section>
 
-        </>
-    );
+            </main>
+            <footer class="">
+              <span>© 2022 Copyright - Programador</span>
+            </footer>
+          </div>
+          );
 }
 
-export default App;
-
-
+          export default App;
