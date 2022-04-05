32dd79ffc861edd3518012072a5913ffe7df6b09
maatheuss0 <matheusaraujoms.machado@gmail.com>
2022-04-05 20:46:24 -0300

    Inicios método login

--- cloudplanning/src/pages/login/login.js
+++ cloudplanning/src/pages/login/login.js
@@ -1,5 +1,8 @@
 import '../../assets/css/login.css'
 import { Component } from 'react';
+import axios from 'axios';
+
+
 
 import logo from '../../assets/img/logo.png'
 
@@ -14,6 +17,17 @@ export default class Login extends Component {
         };
     };
 
+    efetuaLogin = (event) => {
+        
+        event.preventDefault();
+
+        this.setState({ erroMensagem: '', isLoading: true });
+
+        axios.post('http://localhost:5000/api/Login', {
+            email: this.state.email,
+            senha: this.state.senha
+        })
+
 
     render() {
         return (
@@ -26,13 +40,33 @@ export default class Login extends Component {
 
                     <div class="container-input">
                         <a href="http://localhost:3000/">
-                        <img class="logo" src={logo} alt="logo" />
+                            <img class="logo" src={logo} alt="logo" />
                         </a>
                         <p class="titulo-input">Realize login no sistema</p>
                         <hr />
-                        <form class="form-login" onSubmit={this.efetuaLogin}>
-                            <input class="input-login" placeholder="Email" type="email" name="email" required />
-                            <input class="input-login" placeholder="Senha" type="password" name="senha" required />
+                        <form onSubmit={this.efetuaLogin} class="form-login">
+                            <input
+                                className="input-login"
+                                // e-mail
+                                type="text"
+                                name="email"
+                                // define que o input email recebe o valor do state email
+                                value={this.state.email}
+                                // faz a chamada para a função que atualiza o state, conforme o usuário altera o valor do input
+                                onChange={this.atualizaStateCampo}
+                                placeholder="Email"
+                            />
+                            <input
+                                className="input-login"
+                                // senha
+                                type="password"
+                                name="senha"
+                                // define que o input senha recebe o valor do state senha
+                                value={this.state.senha}
+                                // faz a chamada para a função que atualiza o state, conforme o usuário altera o valor do input
+                                onChange={this.atualizaStateCampo}
+                                placeholder="password"
+                            />
                             <span class="esqueceu-senha"> Esqueceu sua senha?</span>
                             <button class="btn-entrar" type="submit">Entrar</button>
                         </form>
