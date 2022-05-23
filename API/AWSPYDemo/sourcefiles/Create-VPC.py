1ad42d8192cdbfc4e95cecbc372f1b9e9ec49f19
Lucas-m3dina <Lucas.m3dina@gmail.com>
2022-05-23 11:12:04 -0300

    Python cors corrigido

--- API/AWSPYDemo/sourcefiles/Create-VPC.py
+++ API/AWSPYDemo/sourcefiles/Create-VPC.py
@@ -7,18 +7,24 @@ myami = 'ami-033594f8862b03bb2'
 myvpc = '10.0.0.0/16'
 
 myvpcid = myclient.create_vpc(
-    CidrBlock=myvpc
+    CidrBlock=myvpc,
 )['Vpc']['VpcId']
 
-myclient.create_subnet(
+myclient.create_tags(Resources=[myvpcid], Tags=[{'Key': 'Name', 'Value': 'Testeeeeeeeeeee'}])
+
+
+subnet1 = myclient.create_subnet(
     CidrBlock='10.0.1.0/24',
     VpcId=myvpcid
-)
+)['Subnet']['SubnetId']
+myclient.create_tags(Resources=[subnet1], Tags=[{'Key': 'Name', 'Value': 'Subnet 1 testeee'}])
 
 mysub = myclient.create_subnet(
     CidrBlock='10.0.2.0/24',
     VpcId=myvpcid
 )['Subnet']['SubnetId']
+
+myclient.create_tags(Resources=[mysub], Tags=[{'Key': 'Name', 'Value': 'Subnet 2 testee'}])
 
 myclient.run_instances(
     ImageId="ami-033594f8862b03bb2",
--- API/AWSPYDemo/sourcefiles/SecurityGroup.py
+++ API/AWSPYDemo/sourcefiles/SecurityGroup.py
@@ -1,5 +1,6 @@
+import pandas as pd
 import boto3
-from flask import Flask, request, Response
+from flask import Flask, request, jsonify, Response
 from flask_cors import CORS, cross_origin
 
 
@@ -7,14 +8,14 @@ app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
-
 @app.route('/api/ec2', methods=['POST'])
 @cross_origin()
 def create_ec2_instance():
     try:
         keyname = request.json['keyname']
         tipoinstancia = request.json['tipoinstancia']
-        idiso = request.json['id-iso']
+        idiso = request.json['idiso']
+
 
         resource_ec2 = boto3.client("ec2")
         resource_ec2.run_instances(
@@ -24,10 +25,10 @@ def create_ec2_instance():
             InstanceType=tipoinstancia,
             KeyName=keyname,
         )
-        return Response("{'msg':'Criado com sucesso'}", status=201, mimetype='application/json')
+        return Response("{'a':'b'}", status=201, mimetype='application/json')
+
     except Exception as e:
         print(e)
-
 
 @app.route('/api/vpc', methods=['POST'])
 @cross_origin()
@@ -39,28 +40,23 @@ def create_vpc():
         ipsubnet2 = request.json['ipsubnet2']
         tipoinstancia = request.json['tipoinstancia']
         keyname = request.json['keyname']
-        nomevpc = request.json['nomevpc']
-        nomesubnet1 = request.json['nomesubnet1']
-        nomesubnet2 = request.json['nomesubnet2']
+
 
         myclient = boto3.client('ec2')
 
         myvpcid = myclient.create_vpc(
             CidrBlock=myvpc
         )['Vpc']['VpcId']
-        myclient.create_tags(Resources=[myvpcid], Tags=[{'Key': 'Name', 'Value': nomevpc}])
 
-        subnet1 = myclient.create_subnet(
+        myclient.create_subnet(
             CidrBlock=ipsubnet1,
             VpcId=myvpcid
-        )['Subnet']['SubnetId']
-        myclient.create_tags(Resources=[subnet1], Tags=[{'Key': 'Name', 'Value': nomesubnet1}])
+        )
 
         mysub = myclient.create_subnet(
             CidrBlock=ipsubnet2,
             VpcId=myvpcid
         )['Subnet']['SubnetId']
-        myclient.create_tags(Resources=[mysub], Tags=[{'Key': 'Name', 'Value': nomesubnet2}])
 
         myclient.run_instances(
             ImageId=myami,
@@ -69,9 +65,9 @@ def create_vpc():
             InstanceType=tipoinstancia,
             KeyName=keyname,
         )
-        return Response("{'msg':'Criado com sucesso'}", status=201, mimetype='application/json')
-    except Exception as e:
-        print(e)
+        return 'teste'
 
+     except Exception as e:
+        print(e)
 
 app.run()
